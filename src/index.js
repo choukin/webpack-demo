cd3885778f0cc1fde8cf5eba673c594131f0901d
zhaoxin <zhaoxin@9fbank.cc>
2018-11-20 14:09:57 +0800

    xml cvs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './head.jpg'
+import Data from './data.xml'
 function component() { 
     var element = document.createElement('div')
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello')
 
+    // 添加图片
+    var myIcon = new Image()
+    myIcon.src = Icon
+
+    element.appendChild(myIcon)
+    console.log(Data)
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,54 @@ module.exports = {
             'style-loader',
             'css-loader'
           ]
-        } 
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            // 'file-loader',
+            { 
+              loader: 'url-loader',
+              options:{
+                limit: 8192,
+                name: '[name].[hash:7].[ext]'
+              }
+            }, 
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false,
+                }
+              }              
+            }       
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use:[
+            'file-loader'
+          ]
+        },
+        {
+          test: /\.(csv|tsv)$/,
+          use:[
+            'csv-loader'
+          ]
+        },        
+        {
+          test: /\.xml$/,
+          use:[
+            'xml-loader'
+          ]
+        }       
       ]
     }
 }
